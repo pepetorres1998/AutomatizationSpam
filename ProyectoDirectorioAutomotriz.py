025e911bbbb420e5f91cf2ec31ee0bb91fc70739
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2018-05-28 23:50:49 -0500

    V1.0

--- ProyectoDirectorioAutomotriz.py
+++ ProyectoDirectorioAutomotriz.py
@@ -6,6 +6,7 @@ import smtplib as sm
 import openpyxl as pxl
 import time
 import sys
+from datetime import datetime
 
 #FUNCIONES UTILES
 
@@ -38,14 +39,13 @@ def useLinks():
 	c_pags = 0
 
 	for i in range(0, len(links_href), 2):
-		print("Iniciando pagina "+str(c_index+(c_pags+1)))
+		print("\tIniciando pagina "+str(c_index+(c_pags+1)))
 		pagina_ = rqs.get(links_href[i], headers = header_rqs)
 		pagina_soup = bf(pagina_.content, 'lxml')
-		print(links_href[i])
-		print("STATUS_Pagina: "+str(pagina_.status_code))
-		print(pagina_.status_code)
-		print(pagina_.request.headers)
-		print("Sleeping")
+		print('\t\t'+links_href[i])
+		print("\t\tSTATUS_Pagina: "+str(pagina_.status_code))
+		print("\t\tSleeping")
+		print('\t\t'+str(datetime.now().time()))
 		time.sleep(5)
 		pagina_datos = pagina_soup.find_all('div', class_='col-md-6')
 		pagina_datos = pagina_datos[1].find('p')
@@ -55,17 +55,17 @@ def useLinks():
 				raise IndexError
 			else:
 				pagina_tabla = pagina_soup.find_all('td')
-				print('TABLA ENCONTRADA')
+				print('\t\t\tTABLA ENCONTRADA')
 				getTable(pagina_tabla, i+1)
 				#print(tablas_company)	#CORRECTO
 				#print(tablas_nombres)	#CORRECTO
 		except IndexError:
-			print("NO HAY TABLA")
+			print("\t\t\tNO HAY TABLA")
 			index_noemail.append(pagina_datos)
 			noemail_names.append(i)
 		index_datos.append(pagina_datos)
 
-		print('DATOS ENCONTRADOS')
+		print('\t\t\tDATOS ENCONTRADOS')
 
 		c_pags = c_pags+1
 
@@ -85,6 +85,7 @@ def doExcel():
 	wb = pxl.Workbook()
 	ws = wb.active
 	ws.title = "Index "+str((c_index/25)+1)
+	max_lenght = 0
 	#CONSTANTES
 	firstFill = pxl.styles.fills.PatternFill(start_color = '00FF0000', end_color = '00FF0000', fill_type = 'solid')
 	secondFill = pxl.styles.fills.PatternFill(start_color = '00FFFF00', end_color = '00FFFF00', fill_type = 'solid')
@@ -94,6 +95,7 @@ def doExcel():
 	sixthFill = pxl.styles.fills.PatternFill(start_color = '000000FF', end_color = '000000FF', fill_type = 'solid')
 	Fills = [secondFill, thirdFill, fourthFill, fifthFill, sixthFill]
 	centerAlignment = pxl.styles.Alignment(horizontal='center', vertical='center', wrapText=True)
+	#centerAlignment = pxl.styles.Alignment(horizontal='center', vertical='center', shrink_to_fit=True)
 	ex_encabezado = ['Empresa', 'Nombre', 'Puesto', 'Correo', 'Telefono']
 	#FOR DE ENCABEZADO
 	for i in range(1, 2):
@@ -124,51 +126,47 @@ def doExcel():
 				current.fill = firstFill
 	try:
 		wb.save('Index '+str((c_index/25)+1)+'.xlsx')
-		print('\tFichero guardado')
+		print('\t\t\tFichero guardado')
 	except:
-		print('ERROR AL GUARDAR')
+		print('\t\t\tERROR AL GUARDAR')
 
 def sendMail():
 	mail_remitente = "jotgatech@gmail.com"
 	mail_mensaje = """
-	<br/><br/>
-	<img src='https://i.imgur.com/btv26hL.png' alt='logo' width='50%'><br/>
-	<p>Reciba un cordial saludo:</p>
-	<p>Este correo fue enviado junto a muchos otros por un sistema dise&ntilde;ado por JOTGA Technologies, el cual recopila contactos de empresas desde un
-	directorio web, y automatiza el proceso de enviar Emails con la finalidad de dar a conocer sus servicios a un enorme n&uacute;mero de clientes
-	potenciales.</p>
-	<p>Adem&aacute;s, crea un registro de manera autom&aacute;tica en Excel, donde incorpora la informaci&oacute;n de contacto a quien se le envi&oacute;
-	un correo. Y genera un documento de texto con los datos de todas las empresas, con la finalidad que pueda contactarlas cuando requiera,
-	teni&eacute;ndolas al alcance de su mano.</p>
-	<p>Si le interesa m&aacute;s informaci&oacute;n acerca de este sistema, responda este correo y nos contactaremos con usted.</p>
-	<ul style="list-style-type:none">
-		<li style="font-size:115%"><img alt='grafica' src='https://images.emojiterra.com/emojione/v2/512px/1f4c8.png' width='15'>Llega a m&aacute;s de 4000 clientes.
-		<li style="font-size:115%"><img alt='mail' src='https://emojipedia-us.s3.amazonaws.com/thumbs/160/facebook/92/e-mail-symbol_1f4e7.png' width='15'>Obt&eacute;n los datos de m&aacute;s de 4000 empresas.
-		<li style="font-size:115%"><img alt='tec' src='http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-11/256/vibration-mode.png' width='15'>Implementa la tecnolog&iacute;a de una manera inteligente.
-	</ul>
-	
-	
-	<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/1000px-F_icon.svg.png' alt='Facebook Logo' width='10' height='10'>
-	<a href="https://www.facebook.com/JOTGATech/">Facebook</a>
-	<p style="font-size:80%">Este correo fue creado y enviado por JOTGA Technologies, a trav&eacute;s de Python.
-	Para informaci&oacute;n o comentarios: jotgatech@gmail.com</p>
-	"""
-	mail_asunto = ""
+<br/><br/>
+<ul style="list-style-type:none">
+	<li style="font-size:125%">Llega a m&aacute;s de 4000 clientes.
+	<li style="font-size:125%">Obt&eacute;n los datos de m&aacute;s de 4000 empresas.
+	<li style="font-size:125%">Implementa la tecnolog&iacute;a de una manera inteligente.
+</ul>
+<p>Visita este link para mejor info: <a href='https://imgur.com/a/FAM2dZK'>Imgur</a>
+</p>
+<p style="font-size:80%">Este correo fue creado y enviado por JOTGA Technologies, a trav&eacute;s de Python.
+Para informaci&oacute;n o comentarios: jotgatech@gmail.com</p>
+"""
+	mail_asunto = "Servicio automatizacion de correos."
 	server = sm.SMTP_SSL('smtp.gmail.com:465')
 	server.ehlo()
 	server.login(mail_remitente, 'panteras_lynx98') #CORRIGE CONTRASEÑA
 	mail_destinatario = tablas_correo
 	mail_email = """From: <%s>
-	To: <%s>
-	MIME-Version: 1.0
-	Content-type: text/html
-	Subject: %s
+To: <%s>
+MIME-Version: 1.0
+Content-type: text/html
+Subject: %s
 
 
-	%s
-	""" % (mail_remitente, mail_destinatario, mail_asunto, mail_mensaje)
+%s
+""" % (mail_remitente, mail_destinatario, mail_asunto, mail_mensaje)
 	server.sendmail(mail_remitente, mail_destinatario, mail_email)
 	server.close()
+
+def sendMailtry():
+	try:
+		sendMail()
+		print('\t\t\tMail sended')
+	except sm.SMTPRecipientsRefused:
+		print('\t\t\tNO HAY EMAIL')
 
 def saveData():
 	with open('Datos Index '+str((c_index/25)+1)+'.txt', 'w') as fila_datos:
@@ -177,7 +175,7 @@ def saveData():
 			fila_datos.write('\n')
 			fila_datos.write(index_datos[i].text)
 			fila_datos.write('\n')
-		print('\tDATOS GUARDADOS')
+		print('\t\t\tDATOS GUARDADOS')
 	with open('Index no email '+str((c_index/25)+1)+'.txt', 'w') as fila_noemail:
 		for i in range(len(index_noemail)):
 			fila_noemail.write(links_nombres[noemail_names[i]+1]+'\n')
@@ -191,7 +189,7 @@ header_rqs = {
 	'User-Agent': 'JOTGA Technologies',
 	'From': 'jotgatech@gmail.com'
 }
-c_index = 0
+c_index = 125
 c_correos = 0
 control = (int(sys.argv[1])-1)*25
 while True:
@@ -207,6 +205,7 @@ while True:
 	index_ = rqs.get('http://www.directorioautomotriz.com.mx/core/Busqueda/Libre/Index/'+indexPage(c_index)+'/', headers = header_rqs)
 	print("Status: "+str(index_.status_code))
 	print(index_.url)
+	print(str(datetime.now().time()))
 	print("Sleeping")
 	time.sleep(5)
 	index_soup = bf(index_.content, 'html.parser')
@@ -224,11 +223,13 @@ while True:
 	#print(links_nombres)
 	useLinks()
 	#FUNCION EXCEL
-	doExcel()
+	if(len(tablas_company)>0):
+		doExcel()
 	c_correos = c_correos + len(tablas_correo)
 	#FUNCION EMAILS
-	#sendMail()
-	#print('\tMail sended')
+	#print(tablas_correo)
+	#sendMailtry()
+	print('\t\t\t'+str(datetime.now().time()))
 	saveData()
 	if(c_index == control):
 		print('FINALIZANDO PROGRAMA')
